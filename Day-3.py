ed7e775fb13405fba239ef8c31322caa7f28add5
Pavan sen <pavansen202008@gmail.com>
2026-07-12 00:43:06 +0530

    comments and escapsequence charater

---
+++ Day-3.py
@@ -0,0 +1,24 @@
+# Today we can dicuss a comment in python programming Language. 
+# A comment is a piece of text that they can not execute in the program.
+
+
+# Comments are used to explain the code
+
+# Comments are two type 
+# 1. single line comment
+# 2. multi line comment
+
+print("Hello world ")
+
+# Zunead is the brillent student of our class. (This is a single line comment)
+
+''' Hi i am kartik This is my journey to learn python programming language. 
+ I want to built my own company in future. I want to be a successful person in my life.
+ so i am working hard to achieve my goal. (this is a multi line comment)''' 
+
+# Today we can also dicuss about what is the scape sequence charater in python
+
+print("Hello sonic \n how are you \n i am fine \n what about you \n i am also fine")
+
+
+
