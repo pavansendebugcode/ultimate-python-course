d23c25be418ed832e0c59a109d215f278287675a
Pavan sen <pavansen202008@gmail.com>
2026-07-13 00:54:17 +0530

    operators

---
+++ Day-6.py
@@ -0,0 +1,33 @@
+# Operator in python programming language
+# Types of operator in python programming language
+# 1. Arithmetic operator ( +, -, *, /, %, **,//)
+# 2. Assignment operator (=, ==, +=, -=, *=, /=, %=, **=, //=)
+# 3. Comparison operator (==, |=, <, >, >=, <=, not equal ot !=)
+# 4. Logical operator (and, or , not)
+# 5. Identity operator (is, is not) 
+
+
+'''Arithmetic operators
+Operator	Operator Name	Example
++	Addition	15+7
+-	Subtraction	15-7
+*	Multiplication	5*7
+**	Exponential	5**3
+/	Division	5/3
+%	Modulus	15%7
+//	Floor Division	15//7'''
+
+n = int(input("enter the value of ; "))
+m = int(input("enter the value of ; "))
+ans1 = n+m
+print("Addition of",n,"and",m,"is", ans1)
+ans2 = n-m
+print("Subtraction of",n,"and",m,"is", ans2)
+ans3 = n*m
+print("Multiplication of",n,"and",m,"is", ans3)
+ans4 = n/m
+print("Division of",n,"and",m,"is", ans4)
+ans5 = n%m
+print("Modulus of",n,"and",m,"is", ans5)
+ans6 = n//m
+print("Floor Division of",n,"and",m,"is", ans6)
