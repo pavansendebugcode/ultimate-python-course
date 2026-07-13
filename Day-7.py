a1e67efe7b68ae0f209b32f6249ac4bb15752b75
Pavan sen <pavansen202008@gmail.com>
2026-07-13 22:58:08 +0530

    create a simple calculator

---
+++ Day-7.py
@@ -0,0 +1,28 @@
+a = int(input('Enter your first number = '))
+b = int(input('Enter your second number = '))
+c = input('What do you want to do (+, -, *, /, %) = ')
+
+if c == "+" :
+    print("The addition is", a + b)
+
+elif c == "-":
+    print("The subtraction is", a - b)
+
+elif c == "*":
+    print("The multiplication is", a * b)
+
+elif c == "/":
+    if b != 0:
+        print("The division is", a / b)
+    else:
+        print("Error: Division by zero is not allowed.")
+
+elif c == "%":
+    if b != 0:
+        print("The remainder is", a % b)
+    else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print("Invalid operation. ")
+
+    
