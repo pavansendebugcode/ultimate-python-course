0281745664b7e0e78eb9a5f490b7495d48876966
Pavan sen <pavansen202008@gmail.com>
2026-07-14 23:31:20 +0530

    This is typecasting

---
+++ Day-8.py
@@ -0,0 +1,30 @@
+# Today topic is what is typecastion in python language
+# Typecasting in python converts the data one type to another data type.
+
+p = 10
+q = 90
+
+sum = p+q
+ 
+print(sum)
+
+
+a = 10
+b = 40.36
+c = "39"
+
+print(type(a))
+
+print(type(b))
+print(type(c))
+
+x = int(b) # converting float to int
+y = float(a) #converting int to float
+z = int(c) # converting string to int
+
+print(type(x) , x )
+print(type(y), y)
+print(type(z), z)
+
+
+
