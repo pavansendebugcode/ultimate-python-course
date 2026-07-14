9f5943f80ed26e4413181c716b393a7d84288e83
Pavan sen <pavansen202008@gmail.com>
2026-07-14 23:49:05 +0530

    Typecating and input function

--- Day-8.py
+++ Day-8.py
@@ -1,13 +1,8 @@
 # Today topic is what is typecastion in python language
 # Typecasting in python converts the data one type to another data type.
-
-p = 10
-q = 90
-
-sum = p+q
- 
-print(sum)
-
+# Typecasting is two types
+ # 1. Explicit Conversion (Explicit type casting in python)
+ #2. Implicit Conversion (Implicit type casting in python)
 
 a = 10
 b = 40.36
@@ -26,5 +21,18 @@ print(type(x) , x )
 print(type(y), y)
 print(type(z), z)
 
+# Today is also learning about the input function in python programming language
+# the input function is used to take input from the user 
+# Input function provides a value as a string data type.
+# But we can convert to other data type using typecasting 
 
+T = input("Enter your name = ")
+print("Hello", T)
 
+# Output 
+# Enter your name = Kartik
+# Hello Kartik
+
+u = input("Enter your date of birth  = ")
+
+print("This date of birth is very funny ", u)
