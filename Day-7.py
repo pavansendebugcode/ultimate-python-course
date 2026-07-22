01240d380338c7e2dbfa0946f5b2925050011839
Pavan sen <pavansen202008@gmail.com>
2026-07-22 23:47:03 +0530

    create a calculator

--- Day-7.py
+++ Day-7.py
@@ -1,8 +1,6 @@
 
 # Question 1: Create a simple calculator 
 
-
-
 a = int(input('Enter your first number = '))
 b = int(input('Enter your second number = '))
 c = input('What do you want to do (+, -, *, /, %) = ')
@@ -29,5 +27,7 @@ elif c == "%":
         print("Error: Division by zero is not allowed.")
 else:
     print("Invalid operation. ")
+
+    print("Try again next time")
 
     
