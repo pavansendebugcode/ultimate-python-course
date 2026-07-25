f2cebfd45b5a326fc109293b91c6814bc75c42b7
Pavan sen <pavansen202008@gmail.com>
2026-07-26 00:28:26 +0530

    what is varabile

--- Day-05.py
+++ Day-05.py
@@ -1,4 +1,4 @@
-'''Day 6 - Variables and Data Types
+'''Day 6 - Variables and Data  Types
 What is a variable?
 Variable is like a container that holds data. Very similar to how our containers in kitchen holds sugar, salt etc Creating a variable is like creating a placeholder in memory and assigning it some value. In Python its as easy as writing:
 
@@ -6,7 +6,7 @@ a = 1
 b = True
 c = "Harry"
 d = None
-These are four variables of different data types.
+These are four variables of  different data types.
 
 What is a Data Type?
 Data type specifies the type of value a variable holds. This is required in programming to do various operations without causing an error.
