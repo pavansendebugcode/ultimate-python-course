20bfabeb0358bc7a8fbefcdea275e929044d5f36
Pavan sen <pavansen202008@gmail.com>
2026-07-26 00:00:44 +0530

    First day of python programming

---
+++ Day--01.py
@@ -0,0 +1,10 @@
+# This is my day first for learning Python programming.
+# I am excited to start this journey and explore the various concepts and features of the language.
+
+#My first program will be a simple "Hello, world"
+print("Hello, world")
+
+#lease note that this is just the beginning, and I will be Learning more about Python programming in the coming days.
+
+# Please good give me power I will complete this journey successfully and become proficient in python programming.
+print('jay shree ram')
