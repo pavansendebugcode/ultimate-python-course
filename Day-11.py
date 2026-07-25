eaf04e540f32b6ed50b5b4a4ca8800c7943f7bd3
Pavan sen <pavansen202008@gmail.com>
2026-07-25 23:34:57 +0530

    Exercise 2

---
+++ Day-11.py
@@ -0,0 +1,20 @@
+# Create a python program to print a good morning , good afernoon , and good evening using a time module 
+Name = input("Enter your name = ")
+import time
+a = time.strftime('%H:%M:%S' )
+print("The current time is ", a)
+b = int(time.strftime('%H'))
+print("The hours is ", b ,"hours")
+c = time.strftime("%M")
+print("The minute is ", c ,"Minute")
+d = time.strftime("%S" )
+print("The second is ", d ,"second")
+
+if b > 1 and b < 12:
+    print( "Good morning", Name , "sir")
+
+elif b < 12 and b <18:
+    print("Good afternoon", Name ," sir")
+
+elif b > 18 and b < 24 :
+    print("Good evening ", Name , "sir")
