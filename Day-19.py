13633de8aad7fe39a91b470c7416c03cf201433d
Pavan sen <pavansen202008@gmail.com>
2026-08-06 12:18:25 +0530

    Exercise 2

--- Day-19.py
+++ Day-19.py
@@ -1,45 +1,153 @@
 # Exercise 2 - create a python program to print a kbc question after question print a how many rupess are win
 
-print
-a = ["who is the prime minister in india", "who is the president of india", 'who is the first president of india' ]
+a = [   ["who is the prime minister in india", 'Narendra modi', 'kartik sen', 'rahul gandi', 'indra gandi', 1,],
 
-print(a[0])
-b = input("what is answer = ")
-if b == 'narendra modi':
-    print('Right answer')
-else:
-    print('wrong answer')
+        ["who is the president of india", 'Dropati murmu' 'Nidhi jain', 'zuned jain', 'sachin', 1],
 
-print(a[1])
-c = input('what is answer = ')
-if c == 'dropati murmu':
-    print('right answer')
-else:
-    print('worng answer')
+        ['who is languae devlop the facebook', 'java scrift', 'python', 'html', 'pp', 1]
+    ]
 
-print(a[2])
-d = input("what is answer = ")
-if d == 'rajendra prasad':
-    print("Right answer")
-else:
-    print('wrong answer')
+level = [1000, 2000, 4000, 8000, 16000]
+money = 0
 
-    # calculation money
+for i in range (0, len(a)):
+    a = a[i]
+    print(f"The question is rs. {level[i]}")
+    print(f"a. {a[1]}             b.{ a[2]}  ")
+    print(f"c. {a[3]}             d. {a[4]}'")
+    reply = int(input('enter your answer - '))
+    if reply == a[-1]:
+        print(f'right answer you have won {level[i]}')
+    if(i == 4):
+      money = 10000
+    elif(i == 9):
+      money = 320000questions = [
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+]
 
-if b == "narendra modi"   and c == "dropati murmu" and d == 'rajendra prasad' :
-    print('you are win 1 cr')
+levels = [1000, 2000, 3000, 5000, 10000, 20000, 40000, 80000, 160000, 320000]
+money = 0
+for i in range(0, len(questions)):
+  
+  question = questions[i]
+  print(f"\n\nQuestion for Rs. {levels[i]}")
+  print(f"a. {question[1]}          b. {question[2]} ")
+  print(f"c. {question[3]}          d. {question[4]} ")
+  reply = int(input("Enter your answer (1-4) or  0 to quit:\n" ))
+  if (reply == 0):
+    money = levels[i-1]
+    break
+  if(reply == question[-1]):
+    print(f"Correct answer, you have won Rs. {levels[i]}")
+    if(i == 4):
+      money = 10000
+    elif(i == 9):
+      money = 32000
+    elif(i == 14):
+      money = 10000000
+  else:
+    print("Wrong answer!")
+    break 
 
-elif b == "narendra modi"   and c == "dropati murmu" :
-    print('you are win 50 laksh')
+print(f"Your take home money is {money}")
+    elif(i == 14):
+      money = 10000000
+    else:
 
-elif b == "narendra modi"  and d == 'rajendra prasad' :
-    print('you are win 50 laksh')
-
-elif c == "dropati murmu" and d == 'rajendra prasad' :
-    print('you are win 52 laksh')
-
-print('you are so lucky you are win large money')
-
-
-
-
+        print("Wrong answer!")
+    
