dca3716fa58691a051345119ae14631d539c4f2f
Pavan sen <pavansen202008@gmail.com>
2026-08-06 23:59:25 +0530

    How to import function realiy work

---
+++ Day-27.py
@@ -0,0 +1,31 @@
+''' Today is i am very tired because today i am not happy
+    Today we are about the import concept in python
+    like How to import work in python how to use]
+    we use a import keyword then they name like math pandas numpy tenserflow
+    
+    as keyword is used to give a name to the module or library in short name
+    from keyword is used to import a specific function from the module or library
+    dir() function is used to see the all function of the module or library'''
+
+
+import math as m
+# from math import sqrt, pi
+# from math import sqrt as a
+
+print(m.tau)
+print(m.pi)
+#print(math.tau(math.pi*2))
+#print(pi)
+print(dir(m))
+
+
+#import kartik
+from kartik import a
+
+
+a()
+
+
+
+
+
