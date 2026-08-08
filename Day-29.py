56e579bbbaa773f916495f30202487aa24932cef
Pavan sen <pavansen202008@gmail.com>
2026-08-08 20:46:36 +0530

    os module

---
+++ Day-29.py
@@ -0,0 +1,17 @@
+# Today is a very special day, so we are learing about the os module in python programming.
+
+# The os module in python medium of communication between the operating system and vs code
+
+# Os module are a built -in module in python
+
+import os 
+
+if (not os.path.exists('kartik')):
+
+    os.mkdir('kartik')
+
+#for i in range(0, 6):
+   # os.mkdir(f'kartik {i+1}')
+
+
+os.rename(f'kartik{i+1}', f'kartiksir{i+1}')
