4a97e26b5265c3cde1bb001ab4e3185a5879898c
Pavan sen <pavansen202008@gmail.com>
2026-08-12 16:52:11 +0530

    class varabile and Instant varabile and static Method

--- Day-37.py
+++ Day-37.py
@@ -1,24 +1,57 @@
-'''Exercise - Write a Library class with no_of_books and books as two instance variables.
- Write a program to create a library from this Library class and show how you can print all books, add a book and get the number of books using different methods. 
- Show that your program doesnt persist the books after the program is stopped!'''
+# Class varabile and Instant varabile ----->  
+#                                             class varabile are define outside the function but inside the class
+#                                             Instant varabile are define inside the funcion and also inside the class
 
 
-class Library():
+class Empoly():
 
-    def __init__(self, no, books):
+    company = 'google'              # class varabile
+    No_of_empoly = 0
 
-        self.no = no
-        self.books = books
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age              # instant varabile
+        self.companynum = 9303
+        Empoly.No_of_empoly += 1
+        
+    def showdetail(self):
+        print(f'The Name of empoly {self.name} and the age of empoly {self.age} and work on {self.company} and the number of empoly is {self.No_of_empoly}')
+  
+    def companyshowdetail(self):
+        print(f'The name of company {self.company} and the number of company is {self.companynum} ')
+ 
+    
+emp1 = Empoly('kartik', 20)
+emp1.showdetail()
+emp1.company =  'Tesla'
+emp1.showdetail()
 
-    def show(self):
-        print(f'No of books are {self.no} and books are {self.books}')
+emp1.companyshowdetail()
 
-x = Library('hindi' 'English')
+emp2 = Empoly('zuned', 45)
+emp2.showdetail()
 
-x.show()
-
-
-
-
+emp3 = Empoly('sachin', 90)
+emp3.company = 'kartik ki company '
+emp3.showdetail()
 
 
+
+# static Method--->
+
+
+class Math():
+
+    def __init__(self, num):
+        self.num = num
+
+    def show(self, n):
+        print(f'The sum of these number is {self.num + n}')
+
+    @staticmethod
+    def add(a,b):
+        return a+b
+
+
+x = Math(5)
+x.show(5)
